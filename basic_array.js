02cee2fe84fd9ddd254dac5b7f765172f87e2fa8
Antur <anturde21@gmail.com>
2025-10-30 23:07:41 +0600

    first commit

---
+++ basic_array.js
@@ -0,0 +1,13 @@
+
+//----------1-------------
+
+var friendAge = [10, 20, 30, 40, 50]
+// If I set friendAge.indexOf(200), than the output will be -1, beacuse if the value is not avaible in the array it will show the output -1;
+
+var position = friendAge.indexOf(20)
+
+var anturAge = friendAge[1]
+
+friendAge[2] = 100;
+
+console.log(friendAge[2]);
