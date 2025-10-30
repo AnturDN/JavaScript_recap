8de3fe1ed8775766f907f7076e463cd1a0865fed
Antur <anturde21@gmail.com>
2025-10-30 23:20:50 +0600

    Added for_loop.js

---
+++ for_loop.js
@@ -0,0 +1,20 @@
+// for (var i = 0; i<=10; i++){
+//     console.log(i);
+// }
+
+var nums = [55, 66, 77, 88, 99, 11, 44];
+
+for(var i = 0; i < nums.length; i++){
+
+    var element = nums[i]
+    console.log(element);
+  
+}
+
+// var friends = ['Roni', 'Sabab','Jamal', 'Kamal', 'Yamal']
+
+// for (var i = 0; i < friends.length; i++){
+//     var friendName = friends[i];
+//     console.log(friendName);
+// }
+
