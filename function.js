473b6ce1586d5f85b302adbffe48a3643a4783d7
Antur <anturde21@gmail.com>
2025-10-30 23:39:57 +0600

    Added function.js

---
+++ function.js
@@ -0,0 +1,29 @@
+// function sayLoveYou(){
+//     console.log('I love you');
+// }
+// sayLoveYou();
+
+// var restaurent = 'Lets go to the restaurent.'
+// var coffe = 'Have some coffe'
+// console.log(restaurent, coffe);
+
+// sayLoveYou()
+
+//---------------------------------------------------------
+
+function doubleIt(num){
+    var result = num*2;
+    return result;
+}
+var first = doubleIt(5)
+var second = doubleIt(100)
+console.log(first, second);
+//---------------------------------------------------------
+function add(num1, num2){
+    var result = num1+num2;
+    return result;
+}
+
+var sum = add(10, 20)
+console.log(sum);
+
