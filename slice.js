55a6de8e606852d45853207abe0c3e3d4ebcf708
Antur <anturde21@gmail.com>
2025-10-31 17:29:12 +0600

    Added slice.js

---
+++ slice.js
@@ -0,0 +1,8 @@
+//---------------3-(slice)------------------------
+
+var teaLine = ['salam', 'balam', 'kalam', 'jalam', 'talam']
+
+var part = teaLine.slice(2, 4) // it will give you the output from 2nd index to the previous element of the 4th index
+
+console.log(part);
+console.log(teaLine);
