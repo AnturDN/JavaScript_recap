5f254ce40a4d4a70a27915a2c527cb80b458bf94
Antur <anturde21@gmail.com>
2025-10-31 16:40:44 +0600

    Added switch.js

---
+++ switch.js
@@ -0,0 +1,15 @@
+num = 100;
+switch(num){
+    case 1000:
+        console.log('I am 1000');
+        break;
+    case 500:
+    case 400:
+        console.log('I am 500');
+        break;
+    case 100:
+        console.log('I am 100!!!');
+        break;
+    default:
+        console.log("I don't find any number");        
+}
