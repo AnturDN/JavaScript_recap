0ac4f6b621e4298d8cde46d8eb7b70c6996577d9
Antur <anturde21@gmail.com>
2025-11-01 18:43:23 +0600

    Added constLet.js

---
+++ constLet.js
@@ -0,0 +1,10 @@
+let name = 'Antur'// we can change the value (if we want) of the variable if we use "let"
+
+const name2 = "Shuvro"// we cannot change the value if we use const
+
+if(name.length > 4){
+    name = 'Joy'
+    name2 = "Nobel"
+}
+
+console.log(name2);
