cfda7f8dfac8e5e3d10cbab4c5760439264c4f85
Antur <anturde21@gmail.com>
2025-11-03 17:50:15 +0600

    Added max.js

---
+++ max.js
@@ -0,0 +1,27 @@
+
+var business = 1050;
+var minister = 650;
+var sochib = 7050;
+
+var max = Math.max(business, minister, sochib )
+console.log(max);
+
+//-------finding maximum number using "if-else"---------
+
+/*** 
+if(business > minister){
+    if(business > sochib){
+        console.log("Business is bigger");
+    }
+    else{
+        console.log("Sochib is bigger");
+    }
+}
+else if(minister > sochib){
+    console.log("Minister is bigger");
+}
+else{
+    console.log("Sochib is bigger");
+}
+*/
+
