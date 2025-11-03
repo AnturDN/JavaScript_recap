8b8d27680b61c1526598d3d469a69c10844eca1b
Antur <anturde21@gmail.com>
2025-11-03 17:49:41 +0600

    Added arraySum.js

---
+++ arraySum.js
@@ -0,0 +1,25 @@
+
+
+var numbers = [45, 65, 78, 12, 3, 54, 65];
+var sum = 0;
+for(var i = 0; i < numbers.length; i++){
+    var element = numbers[i];
+    sum = sum + element;
+}
+console.log("Total sum : ", sum);
+
+//---------using function---------
+
+var numbers = [45, 65, 78, 12, 3, 54, 65];
+
+function getArraySum(numbers){
+    var sum = 0;
+    for(var i = 0; i < numbers.length; i++){
+        var element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+
+}
+var result = getArraySum(numbers)
+console.log("Total sum : ", result);
